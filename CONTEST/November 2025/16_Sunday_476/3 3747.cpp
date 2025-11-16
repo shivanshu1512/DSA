a9ad51d2787033ca14a3e691c0fda54c2f3ccd06
shivanshu1512 <meshivanshu1512@gmail.com>
2025-11-16 20:27:37 +0530

    Implement countDistinct function to calculate distinct numbers based on input

--- CONTEST/November 2025/16_Sunday_476/3 3747.cpp
+++ CONTEST/November 2025/16_Sunday_476/3 3747.cpp
@@ -5,14 +5,11 @@ public:
         int len = s.length();
         long long result = 0;
         long long power = 9;
-
         for (int i = 1; i < len; i++) {
             result += power;
             power *= 9;
         }
-
         bool isValid = true;
-
         for (int i = 0; i < len; i++) {
             int digit = s[i] - '0';
             if (digit == 0) {
