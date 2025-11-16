ffd97782e7156740e2dfeae59b8fe441604da0a4
shivanshu1512 <shivanshu1512@gmail.com>
2025-11-16 17:22:20 +0530

    Implement numberOfSubstrings function to calculate valid substrings based on zero and one counts

---
+++ POTD/November 2025/16 1513.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int numSub(string s) {
+        const long long MOD = 1000000007;  
+        long long ans = 0;
+        long long count1 = 0;
+        for (char ch : s) {
+            if (ch == '1') {
+                count1++;
+                ans = (ans + count1) % MOD;
+            } else {
+                count1 = 0;
+            }
+        }
+
+        return ans; // n*n+1/2 also a formula which we can apply
+        
+    }
+};
